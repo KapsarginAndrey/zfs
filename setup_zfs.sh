7c3940b418f9d28d7d5f9ffbaa0b15289a4915cc
kapsar <kapsarginae@gmail.com>
2021-05-14 00:22:24 +0300

    first

---
+++ setup_zfs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ~root/.ssh
+cp ~vagrant/.ssh/auth* ~root/.ssh
+sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
+yum install -y yum-utils
+
+sudo yum -y install http://download.zfsonlinux.org/epel/zfs-release.el8_2.noarch.rpm
+gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+yum-config-manager --enable zfs-kmod
+yum-config-manager --disable zfs
+yum install -y zfs
+modprobe zfs
+
+
+
